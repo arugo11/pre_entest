918b1b969be1aa698c64c70e124740b30ed8fbf4
arugo11 <neziminoarugo1111@gmail.com>
2021-10-27 17:02:49 +0900

    record生成だめです

---
+++ record_init.py
@@ -0,0 +1,14 @@
+import csv
+
+
+with open("data/record.csv" , "w") as f:
+    for i in range (1 , 1901):
+        writer = csv.writer(f)
+        writer.writerow([i , 0 , 0 , ""])#リセット条件
+
+
+
+
+
+
+
--- renban.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-
-with open("record.csv" , "w") as f:
-    for i in range (1 , 1901):
-        writer = csv.writer(f)
-        writer.writerow([i])
-
-
